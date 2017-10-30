38c345bc763c0d04d535e2ea9f55187aba0a571a
Krzysztof Dobrzyński <krdobr@netwise.pl>
2017-10-30 14:06:15 +0100

    Added ReplaceAt image

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -14,8 +14,13 @@ namespace Tests
 
             Console.WriteLine();
 
-            list = list.ReplaceAt(1, 9).ReplaceAt(2, 567);
-            list.ToList().ForEach(model => Console.Write(model + ", "));
+            var list2 = list.ReplaceAt(1, 9).ToList();
+            list2.ToList().ForEach(model => Console.Write(model + ", "));
+
+            Console.WriteLine();
+
+            var list3 = list2.ReplaceAt(2, 567).ToList();
+            list3.ToList().ForEach(model => Console.Write(model + ", "));
 
             Console.ReadKey();
         }
