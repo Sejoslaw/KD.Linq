848e28db4aabf1ba17035a2cf0e1d4204afe7bba
Krzysztof Dobrzyński <krdobr@netwise.pl>
2017-10-31 14:29:23 +0100

    Cleaned up ForEach example

--- Test_ForEach/Program.cs
+++ Test_ForEach/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using KD.Linq;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Test_ForEach
 {
@@ -9,11 +11,13 @@ namespace Test_ForEach
         {
             List<int> list = new List<int>() { 1, 2, 1, 3, 1, 4 };
             // Standard ForEach which is include in List class
-            Console.WriteLine(string.Join(", ", list));
+            list.ForEach(value => Console.Write(value + ", "));
+
+            Console.WriteLine();
 
             IEnumerable<int> enumerable = new int[] { 1, 2, 1, 3, 1, 4 };
             // Generic ForEach made for any Enumerable
-            Console.WriteLine(string.Join(", ", enumerable));
+            enumerable.ForEach(value => Console.Write(value + ", ")).ToList();
 
             Console.ReadKey();
         }
